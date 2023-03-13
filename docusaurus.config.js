0d14b8f20246c441b43fe2d3e750015ee20b55c7
JenniBeadle <jenni_beadle@sil.org>
2023-03-13 21:01:52 +0800

    still trying es

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
         // ... Your options.
         // `hashed` is recommended as long-term-cache of index file is possible.
         hashed: true,
-        language: ["en", "fr"],
+        language: ["en", "fr", "es"],
         indexBlog: false,
         indexPages: false,
         docsRouteBasePath: "/",
