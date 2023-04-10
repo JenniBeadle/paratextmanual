24468b1b8237cfac62af2c9918d5a1059ba1a56d
JenniBeadle <jenni_beadle@sil.org>
2023-04-10 18:24:33 +0800

    Deploy website - based on a3a6d70e2cef76ac4c1cd11608a56b06a24c6075 [formerly ae784c6912195d7fe980f5814cd6f8d762f86810]
    Former-commit-id: 5256ea01b4cdf13ef2506def48d8c9330aaedbd5

---
+++ pt/assets/js/9ca0a406.d5678ec4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkparatextmanual=self.webpackChunkparatextmanual||[]).push([[4039],{83769:a=>{a.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
