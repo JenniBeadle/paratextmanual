fa65546c03278bfce540464b42205f2dbf6a17db
JenniBeadle <jenni_beadle@sil.org>
2023-06-24 02:19:35 +0800

    update
    Former-commit-id: e7efb996ba9711259aac81b328645a3f3fb2a2ae

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,10 +19,12 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'JenniBeadle', // Usually your GitHub org/user name.
   projectName: 'paratextmanual', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
+ 
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'fr', 'es', 'id', 'pt', 'ru', 'hi', 'ar', 'ml', 'zh-CN', 'zh-TW'], 
